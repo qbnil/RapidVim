798fd4cc28204a29af37a5a6d96ecc9ed12eb393
inilov <tomederson92@gmail.com>
2024-05-24 17:39:01 +0300

    RapidVim v3 - got treesitter up and working

--- lua/rapidvim/plugins/rose-pine.lua
+++ lua/rapidvim/plugins/rose-pine.lua
@@ -2,14 +2,12 @@ return {
     "rose-pine/neovim",
     name = "rose-pine",
     lazy = false,
+    priority = 1000,
     config = function()
         require('rose-pine').setup({
-            variant = "main", -- auto, main, moon, or dawn
-            dark_variant = "main", -- main, moon, or dawn
             disable_italics = true,
-            dim_inactive_windows = false,
             extend_background_behind_borders = true,
-            vim.cmd("colorscheme rose-pine")
+            vim.cmd("colorscheme rose-pine-main")
         })
     end
 }
--- lua/rapidvim/plugins/treesitter.lua
+++ lua/rapidvim/plugins/treesitter.lua
@@ -1,52 +1,17 @@
 return {
-    -- Treesitter is a new parser generator tool that we can
-    -- use in Neovim to power faster and more accurate
-    -- syntax highlighting.
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    event = "VeryLazy",
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    ---@diagnostic disable-next-line: missing-fields
-    opts = {
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-            "bash",
-            "c",
-            "diff",
-            "html",
-            "javascript",
-            "jsdoc",
-            "json",
-            "jsonc",
-            "lua",
-            "luadoc",
-            "luap",
-            "markdown",
-            "markdown_inline",
-            "python",
-            "query",
-            "vimdoc",
-            "yaml",
-        },
-        incremental_selection = {
-            enable = true,
-            keymaps = {
-                init_selection = "<C-space>",
-                node_incremental = "<C-space>",
-                scope_incremental = false,
-                node_decremental = "<bs>",
-            },
-        },
-        textobjects = {
-            move = {
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+        local treesitter = require("nvim-treesitter.configs")
+        treesitter.setup({
+            highlight = {
                 enable = true,
-                goto_next_start = { ["]f"] = "@function.outer", ["]c"] = "@class.outer" },
-                goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer" },
-                goto_previous_start = { ["[f"] = "@function.outer", ["[c"] = "@class.outer" },
-                goto_previous_end = { ["[F"] = "@function.outer", ["[C"] = "@class.outer" },
+                disable = { "json" },
             },
-        },
-    },
+            ensure_installed = {
+                "lua",
+            },
+        })
+    end
 }
