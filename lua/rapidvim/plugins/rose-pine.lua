c140e142a047af806fc2e506338fada88518ce36
qbnil <tomederson92@gmail.com>
2024-05-25 20:02:58 +0300

    RapidVim v3.1 - colorscheme update, changing highlights

--- lua/rapidvim/plugins/rose-pine.lua
+++ lua/rapidvim/plugins/rose-pine.lua
@@ -13,12 +13,13 @@ return {
             highlight_groups = {
                 Comment = { fg = "#67616e" },
                 -- VertSplit = { fg = "muted", bg = "muted" },
-                Keyword = { fg = "#256148"},
+                Keyword = { fg = "#667b77" },
+                Visual = { bg = "#434343" },
             },
         })
         vim.cmd('colorscheme rose-pine-main')
         vim.cmd('colorscheme rose-pine-main')
-        vim.api.nvim_set_hl(0, 'Pmenu', { bg = '#262626', fg = "#cfccd1" })
-        vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#312f2f' })
+        vim.api.nvim_set_hl(0, 'Pmenu', { bg = '#272727' })
+        vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#525252' })
     end
 }
