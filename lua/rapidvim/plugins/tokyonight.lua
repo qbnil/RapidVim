90f2fad392ee45795299b8a40e2fbb95010e6a01
inilov <tomederson92@gmail.com>
2024-06-07 18:23:22 +0300

    RapidVim v4.7.2 - tokyonight-theme file modified

--- lua/rapidvim/plugins/tokyonight.lua
+++ lua/rapidvim/plugins/tokyonight.lua
@@ -40,5 +40,8 @@ return {
 			on_highlights = function(highlights, colors) end,
 		})
 		vim.cmd([[colorscheme tokyonight-night]])
+		-- vim.cmd([[colorscheme tokyonight]])
+		-- vim.cmd([[colorscheme tokyonight-day]])
+		-- vim.cmd([[colorscheme tokyonight-moon]])
 	end,
 }
