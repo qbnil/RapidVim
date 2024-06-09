b1132a20ac5bf44b4c7155c520ac2dc1fe3dc1ac
qbnil <tomederson92@gmail.com>
2024-06-10 00:25:21 +0300

    RapidVim v4.9 - some changes added

--- lua/rapidvim/plugins/gitsigns.lua
+++ lua/rapidvim/plugins/gitsigns.lua
@@ -12,8 +12,8 @@ return {
                 untracked = { text = "┆" },
             },
             signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-            numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-            linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+            numhl = false,     -- Toggle with `:Gitsigns toggle_numhl`
+            linehl = false,    -- Toggle with `:Gitsigns toggle_linehl`
             word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
             watch_gitdir = {
                 follow_files = true,
@@ -86,7 +86,7 @@ return {
             },
             sign_priority = 6,
             update_debounce = 100,
-            status_formatter = nil, -- Use default
+            status_formatter = nil,  -- Use default
             max_file_length = 40000, -- Disable if file is longer than this (in lines)
             preview_config = {
                 -- Options passed to nvim_open_win
@@ -97,5 +97,9 @@ return {
                 col = 1,
             },
         })
+        vim.api.nvim_set_hl(0, "GitSignsAdd", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "GitSignsChange", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "GitSignsDelete", { bg = "NONE" })
+        vim.api.nvim_set_hl(0, "GitSignsChangeDelete", { bg = "NONE" })
     end,
 }
