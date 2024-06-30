7d2e2d11bdde98843890f76216b7b5978cd8f265
qbnil <tomederson92@gmail.com>
2024-06-30 21:40:06 +0300

    RapidVim v6.4 - sad-replacer config changes

--- lua/rapidvim/plugins/sad-replacer.lua
+++ lua/rapidvim/plugins/sad-replacer.lua
@@ -5,13 +5,14 @@ return {
     config = function()
         require("sad").setup {
             debug = false, -- print debug info
-            diff = 'delta', -- you can use `less`, `diff-so-fancy`
+            diff = 'diff-so-fancy', -- you can use `less`, `diff-so-fancy`
             ls_file = 'fd', -- also git ls-files
             exact = true, -- exact match
-            vsplit = 1, -- split sad window the screen vertically, when set to number
+            vsplit = false, -- split sad window the screen vertically, when set to number
             -- it is a threadhold when window is larger than the threshold sad will split vertically,
             height_ratio = 0.6, -- height ratio of sad window when split horizontally
             width_ratio = 0.6, -- height ratio of sad window when split vertically
         }
+        vim.keymap.set('n', '<leader>r', vim.cmd.Sad)
     end,
 }
