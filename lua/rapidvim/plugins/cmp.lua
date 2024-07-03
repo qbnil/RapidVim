23b2019352eec3162fc71b6a7937a3ea28447f10
qbnil <tomederson92@gmail.com>
2024-07-04 01:15:43 +0300

    RapidVim v7.2 - cmp.lua dependencies modification

--- lua/rapidvim/plugins/cmp.lua
+++ lua/rapidvim/plugins/cmp.lua
@@ -4,7 +4,6 @@ return {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-path",
         "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-nvim-lsp-signature-help",
         {
             "L3MON4D3/LuaSnip",
             build = "make install_jsregexp",
