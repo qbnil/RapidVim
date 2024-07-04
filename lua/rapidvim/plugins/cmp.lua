abdc87b66c9c273070022f47c63f54d0e6334023
inilov <tomederson92@gmail.com>
2024-07-04 13:45:39 +0300

    RapidVim v7.3 - cmp-nvim-lua added

--- lua/rapidvim/plugins/cmp.lua
+++ lua/rapidvim/plugins/cmp.lua
@@ -1,99 +1,101 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-buffer",
-        {
-            "L3MON4D3/LuaSnip",
-            build = "make install_jsregexp",
-            dependencies = {
-                "saadparwaiz1/cmp_luasnip",
-                "rafamadriz/friendly-snippets",
-            },
-        },
-    },
-    config = function()
-        -- require "custom.completion"
-        -- require("luasnip.loaders.from_snipmate").lazy_load()
-        require("luasnip.loaders.from_vscode").lazy_load()
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-nvim-lua",
+		{
+			"L3MON4D3/LuaSnip",
+			build = "make install_jsregexp",
+			dependencies = {
+				"saadparwaiz1/cmp_luasnip",
+				"rafamadriz/friendly-snippets",
+			},
+		},
+	},
+	config = function()
+		-- require "custom.completion"
+		-- require("luasnip.loaders.from_snipmate").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        vim.opt.completeopt = { "menu", "menuone", "noselect" }
-        vim.opt.shortmess:append("c")
+		vim.opt.completeopt = { "menu", "menuone", "noselect" }
+		vim.opt.shortmess:append("c")
 
-        local cmp = require("cmp")
+		local cmp = require("cmp")
 
-        cmp.setup({
-            preselect = 'item',
-            completion = {
-                completeopt = 'menu,menuone,noinsert'
-            },
-            formatting = {
-                fields = { 'abbr', 'kind', 'menu' },
-                -- here is where the change happens
-                format = function(entry, item)
-                    local menu_icon = {
-                        nvim_lsp = '[Lsp]',
-                        luasnip = '[Snip]',
-                        buffer = '[Buf]',
-                        path = '[Path]',
-                        nvim_lua = '[Lua]',
-                    }
-                    item.menu = menu_icon[entry.source.name]
-                    return item
-                end,
-            },
-            sources = {
-                { name = "nvim_lsp" },
-                { name = "luasnip" },
-                { name = "path" },
-                { name = "buffer" },
-            },
-            appearance = {
-                menu = {
-                    direction = 'auto'
-                }
-            },
-            mapping = {
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif require("luasnip").expand_or_jumpable() then
-                        vim.fn.feedkeys(
-                            vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
-                            ""
-                        )
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
+		cmp.setup({
+			preselect = "item",
+			completion = {
+				completeopt = "menu,menuone,noinsert",
+			},
+			formatting = {
+				fields = { "abbr", "kind", "menu" },
+				-- here is where the change happens
+				format = function(entry, item)
+					local menu_icon = {
+						nvim_lsp = "[Lsp]",
+						luasnip = "[Snip]",
+						buffer = "[Buf]",
+						path = "[Path]",
+						nvim_lua = "[Lua]",
+					}
+					item.menu = menu_icon[entry.source.name]
+					return item
+				end,
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "path" },
+				{ name = "buffer" },
+				{ name = "nvim_lua" },
+			},
+			appearance = {
+				menu = {
+					direction = "auto",
+				},
+			},
+			mapping = {
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						vim.fn.feedkeys(
+							vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true),
+							""
+						)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif require("luasnip").jumpable(-1) then
-                        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-            },
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			},
 
-            -- Enable luasnip to handle snippet expansion for nvim-cmp
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body)
-                end,
-            },
-        })
+			-- Enable luasnip to handle snippet expansion for nvim-cmp
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+		})
 
-        local ls = require("luasnip")
-        ls.config.set_config({
-            history = false,
-            updateevents = "TextChanged,TextChangedI",
-        })
-    end,
+		local ls = require("luasnip")
+		ls.config.set_config({
+			history = false,
+			updateevents = "TextChanged,TextChangedI",
+		})
+	end,
 }
