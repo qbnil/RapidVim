6d5650f29bef65a63256c56d1461bd33942d5f16
qbnil <tomederson92@gmail.com>
2024-07-09 01:49:12 +0300

    RapidVim v7.6 - formatters added + formatting config changed a little bit

--- lua/rapidvim/plugins/conform.lua
+++ lua/rapidvim/plugins/conform.lua
@@ -1,22 +1,32 @@
 return {
-    "stevearc/conform.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    config = function()
-        local conform = require("conform")
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+        -- Setup
+		local conform = require("conform")
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang_format" },
+				cpp = { "clang_format" },
+			},
+			format_on_save = false,
+		})
 
-        conform.setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-            },
-            format_on_save = false,
-        })
+        -- Formatters style
+		conform.formatters.clang_format = {
+			prepend_args = function()
+				return { "--style={IndentWidth: 4}" }
+			end,
+		}
 
-        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-            conform.format({
-                lsp_fallback = true,
-                async = false,
-                timeout_ms = 1000,
-            })
-        end, { desc = "Format file or range (in visual mode)" })
-    end,
+        -- Keymaps
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
 }
